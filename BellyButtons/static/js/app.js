7843f29c6b4414f6dc11b7cd81bf4d6e7942f33f
Anthony <70252860+anthonyvanderwal@users.noreply.github.com>
2020-12-13 11:34:40 +0800

    choose id and restyle

--- BellyButtons/static/js/app.js
+++ BellyButtons/static/js/app.js
@@ -1,75 +1,124 @@
-// helper function for unpacking json string
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-}
-
-// load data from json file supplied
+// get data from json
 d3.json('./static/data/samples.json').then( d => {
 
+    // list of participant ids
+    var ids = d.names;
+
     // data
-    var onePerson = {
-        'value': unpack(d.samples, 'sample_values')[0],
-        'id': unpack(d.samples, 'otu_ids')[0],
-        'label': unpack(d.samples, 'otu_labels')[0]
-    };
+    var data = {
+        'value': unpack(d.samples, 'sample_values'),
+        'id': unpack(d.samples, 'otu_ids'),
+        'label': unpack(d.samples, 'otu_labels'),
+        'demographics': d.metadata,
+    };   
 
-    // responsive charts
-    var config = {responsive: true}
+    // populate list of id to choose from
+    idList(ids);
 
-    // bar chart
-    var barData = [{
-        type: 'bar',
-        orientation: 'h',
-        x: onePerson.value.slice(0,10).reverse(),
-        y: onePerson.id.slice(0,10).reverse().map( i => 'OTU ' + i.toString() ),
-        text: onePerson.label.slice(0,10).reverse(),
-        marker: { color: '#0090ff' }
-    }];
-    
-    var barLayout = {
-        xaxis: { title: 'Value'},
-        yaxis: { title: 'Microbe'},
-        plot_bgcolor: 'lightgrey',
-        paper_bgcolor: 'lightgrey',
-        bargap: 0.05,
-        margin: { l: 100, r: 10, b: 60, t: 10, pad: 5 }
-    };
-    
-    Plotly.newPlot('bar', barData, barLayout, config);
-      
-    // bubble chart
-    var bubbleData = [{
-        type: 'bubble',
-        mode: 'markers',
-        x: onePerson.id,
-        y: onePerson.value,
-        text: onePerson.label,
-        marker: { 
-            size: onePerson.value, 
-            color: onePerson.id,
-            colorscale: 'Jet'
-        }
-    }];
-    
-    var bubbleLayout = {
-        xaxis: { title: 'Microbe ID'},
-        yaxis: { title: 'Value'},
-        plot_bgcolor: 'lightgrey',
-        paper_bgcolor: 'lightgrey',
-        margin: { l: 50, r: 10, b: 50, t: 10, pad: 5 }
-    };
-      
-    Plotly.newPlot('bubble', bubbleData, bubbleLayout, config);
+    // populate dropdown list with participant id
+    function idList(data) {
+        var dropList = d3.select('#selDataset');
+        dropList.selectAll('option')
+            .data(data)
+            .enter()
+            .append('option')
+                .attr( 'value', d => d )
+                .text( d => d );
+    }
 
-    // info panel
-    var onePersonDem = d.metadata[0];
-    Object.entries(onePersonDem).forEach( v => {
-        d3.select('#sample-metadata')
-        .append('text')
-        .text(`${v[0].toUpperCase()} : ${v[1]}`)
-        .append('br');
-    });
+    // populate site with an initial set of data
+    init(data.value[0], data.id[0], data.label[0], data.demographics[0]);
+
+    // function calls on the first set of data '[0]'
+    function init(value, id, label, demographics) {
+
+        // responsive charts
+        var config = {responsive: true}
+
+        // bar chart
+        var barData = [{
+            type: 'bar',
+            orientation: 'h',
+            x: value.slice(0,10).reverse(),
+            y: id.slice(0,10).reverse().map( i => 'OTU ' + i.toString() ),
+            text: label.slice(0,10).reverse(),
+            marker: { color: '#0090ff' }
+        }];
+        var barLayout = {
+            xaxis: { title: 'Value'},
+            yaxis: { title: 'Microbe'},
+            plot_bgcolor: 'lightgrey',
+            paper_bgcolor: 'lightgrey',
+            bargap: 0.05,
+            margin: { l: 100, r: 10, b: 60, t: 10, pad: 5 }
+        };
+        Plotly.newPlot('bar', barData, barLayout, config);
+        
+        // bubble chart
+        var bubbleData = [{
+            type: 'bubble',
+            mode: 'markers',
+            x: id,
+            y: value,
+            text: label,
+            marker: { 
+                size: value, 
+                color: id,
+                colorscale: 'Jet'
+            }
+        }];
+        var bubbleLayout = {
+            xaxis: { title: 'Microbe ID'},
+            yaxis: { title: 'Value'},
+            plot_bgcolor: 'lightgrey',
+            paper_bgcolor: 'lightgrey',
+            margin: { l: 50, r: 10, b: 50, t: 10, pad: 5 }
+        };
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout, config);
+
+        // info panel
+        Object.entries(demographics).forEach( v => {
+            d3.select('#sample-metadata')
+            .append('text')
+            .text(`${v[0].toUpperCase()} : ${v[1]}`)
+            .append('br');
+        });
+    }
+
+    // listen for changes to 'select' tag and update plots/info
+    d3.selectAll('#selDataset').on('change', update);    
+
+    //  restyle plots and info box with selected id info
+    function update() {
+    
+        var key = Object.keys(ids).find( key => ids[key] == this.value) ;
+        var newId = data.id[key];
+        var newValue = data.value[key];
+        var newDemo = data.demographics[key];
+        
+        // bar chart
+        Plotly.restyle('bar', 'x', [newValue.slice(0,10).reverse()]);
+        Plotly.restyle('bar', 'y', [newId.slice(0,10).reverse().map( i => 'OTU ' + i.toString() )]);
+
+        // bubble chart
+        Plotly.restyle('bubble', 'x', [newId]);
+        Plotly.restyle('bubble', 'y', [newValue]);
+
+        // info panel
+        d3.selectAll('#sample-metadata text').remove();
+        Object.entries(newDemo).forEach( v => {
+            d3.select('#sample-metadata')
+            .append('text')
+            .text(`${v[0].toUpperCase()} : ${v[1]}`)
+            .append('br');
+        });
+    }
+
+    // helper function for unpacking json string
+    function unpack(rows, index) {
+        return rows.map(function(row) {
+            return row[index];
+        });
+    }
 
 });
